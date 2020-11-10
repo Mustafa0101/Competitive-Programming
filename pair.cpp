03cdac08fd18390cb12e6f825034a7ee4ef780d3
Mustafa0101 <74010999+Mustafa0101@users.noreply.github.com>
2020-11-10 22:55:36 +0530

    Create pair.cpp

---
+++ pair.cpp
@@ -0,0 +1,14 @@
+ // collection of two values
+ // Pair Syntax: pair<data_type1, data_type2> pair_name;
+ // Way of declaration
+   pair<int, string>p1;
+  pair<int, string>p2 {15, "sachin"};
+   pair<int, string>p3 = make_pair(15, "sachin");
+  
+   p1.first = 5;
+   p1.second = "sachin";
+  
+ // printing values
+  cout<<p2.first<<" ";
+  cout<<p2.second<<" ";
+  
